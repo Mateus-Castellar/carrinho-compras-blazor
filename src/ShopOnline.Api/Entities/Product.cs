bfc977989f5068f5b5e11e3b1c2715653022194b
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-22 23:34:46 -0300

    feat: Adicionando entidades na api

---
+++ src/ShopOnline.Api/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
