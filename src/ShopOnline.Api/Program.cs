47a5d38a361f69319db74fb6f6d6f5936f55fb52
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-28 23:13:21 -0300

    feat: Pagina de listagem de produtos consumindo backend

--- src/ShopOnline.Api/Program.cs
+++ src/ShopOnline.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using ShopOnline.Api.Data;
 using ShopOnline.Api.Repositories;
 
@@ -20,6 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(policy =>
+    policy.WithOrigins("https://localhost:7155", "http://localhost:7155")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType));
 
 app.UseHttpsRedirection();
 
---
+++ src/ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var products = await _httpClient
+                    .GetFromJsonAsync<IEnumerable<ProductDto>>("api/products");
+
+                return products;
+            }
+            catch (Exception)
+            {
+                //log exceptions
+                throw;
+            }
+        }
+    }
+}
