087fb1fd038f745f1af909077b64fe1519fe0c7e
Mateus Castellar <mateuscastellar@outlook.com>
2022-11-01 02:42:34 -0300

    feat: Servico de carrinho de compras na app web

---
+++ src/ShopOnline.Web/Services/ShoppingCartService.cs
@@ -0,0 +1,68 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ShoppingCartService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CartItemDto> AddItem(CartItemtoAddDto cartItemtoAddDto)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync<CartItemtoAddDto>
+                    ("api/shoppingCarts", cartItemtoAddDto);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == HttpStatusCode.NoContent)
+                        return default(CartItemDto);
+
+                    return await response.Content.ReadFromJsonAsync<CartItemDto>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"http status {response.StatusCode} Message {message}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<CartItemDto>> GetItems(int userId)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"api/{userId}/GetItems");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == HttpStatusCode.NoContent)
+                        return Enumerable.Empty<CartItemDto>();
+
+                    return await response.Content.ReadFromJsonAsync<IEnumerable<CartItemDto>>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"http status {response.StatusCode} Message {message}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
